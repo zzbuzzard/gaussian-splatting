237be5613c470d85da7fe227555d7cf07ebe0b51
zzb <40760215+zzbuzzard@users.noreply.github.com>
2023-11-09 16:11:50 +0000

    Noisy dataset makes more sense

--- noisy_dataset/noisy_dataset.py
+++ noisy_dataset/noisy_dataset.py
@@ -5,6 +5,7 @@ import torch.utils.data as dutils
 from typing import List
 from PIL import Image
 import os
+from os.path import join
 import argparse
 import shutil
 from tqdm import tqdm
@@ -13,61 +14,45 @@ from tqdm import tqdm
 class NoisyDataset(dutils.Dataset):
     """
     Indexing gives (ground truth, rendered) Tensor pairs.
-    Cropping and flipping are implemented without torch.transforms, as we have to make sure the same transformation
-    is applied to the gt and rendered image.
+    Data augmentation: gives option to crop, flip.
     """
 
-    def __init__(self, root_path: str, iters: List[int] = None, crop_size=(512, 512), flipx=True, flipy=True,
-                 transform=lambda x: x, scenes=None):
-        def path_list(scene, test_or_train, iters):
-            x = "gt" if iters is None else f"render_{iters}"
-            path = os.path.join(root_path, scene, x, test_or_train)
-            return [os.path.join(path, i) for i in os.listdir(path)]
-
-        self.root_path = root_path
-        self.iters = iters
+    def __init__(self, root_path: str, split: str, crop_size=(512, 512), flipx=True, flipy=True):
+        self.root_path = join(root_path, split)
         self.crop_size = crop_size
         self.flipx = flipx
         self.flipy = flipy
-        self.transform = transform
 
         assert os.path.isdir(root_path)
 
-        render_paths = []
-        gt_paths = []
+        gt_root = join(root_path, "gt")
+        self.gt_paths = [join(gt_root, i) for i in os.listdir(gt_root)]
 
-        # e.g. gt_paths = ["a", "b", "c"]
-        #   render_paths = [["a1", "a2", "a3"], ...]
+        render_root = join(root_path, "render")
 
-        # [a1, b1, c1]
-        # [a2, b2, c2]
+        d1 = os.listdir(render_root)[0]
 
-        for scene in os.listdir(root_path):
-            # If scenes is specified, skip scene if not in the list
-            if scenes is not None and scene not in scenes:
-                continue
-            gt_paths += path_list(scene, "train", None)
-            gt_paths += path_list(scene, "test", None)
-
-            rpaths = []
-            for i in os.listdir(os.path.join(root_path, scene)):
-                if i.startswith("render_"):
-                    it = int(i[7:])
-                    if iters is None or it in iters:
-                        rpaths.append(path_list(scene, "train", it) + path_list(scene, "test", it))
-
-            render_paths += list(zip(*rpaths))
-
-        self.gt_paths = gt_paths
-        self.render_paths = render_paths
+        # Multiple
+        if os.path.isdir(join(render_root, d1)):
+            paths = []
+            for d in os.listdir(render_root):
+                path = join(render_root, d)
+                paths.append([join(path, i) for i in os.listdir(path)])
+            lengths = [len(i) for i in paths]
+            assert len(set(lengths)) == 1, f"Directories in {render_root} have different file counts: {lengths}"
+            self.render_paths = zip(*lengths)
+            self.n = lengths[0]
+        # Single
+        else:
+            self.render_paths = [[join(render_root, i)] for i in os.listdir(render_root)]
+            self.n = 1
 
     def __len__(self):
-        return len(self.render_paths)
+        return len(self.gt_paths)
 
     def __getitem__(self, item):
         """Returns (gt, im) both of shape 3 x H x W where (H, W) = self.crop_size."""
-        n = len(self.render_paths[item])
-        rp = self.render_paths[item][torch.randint(0, n, ())]
+        rp = self.render_paths[item][torch.randint(0, self.n, ())]
         gp = self.gt_paths[item]
 
         tot = transforms.ToTensor()
@@ -101,7 +86,67 @@ class NoisyDataset(dutils.Dataset):
             rim = trf.vflip(rim)
             gim = trf.vflip(gim)
 
-        return self.transform(gim), self.transform(rim)
+        return gim, rim
+
+
+def construct_subset_dataset(root_path: str, out_path: str, split_every_n: int, iters: List[int] = None, scenes=None):
+    """Create a 'subset' dataset containing only some scenes and iteration counts"""
+    def path_list(scene, test_or_train, iters):
+        x = "gt" if iters is None else f"render_{iters}"
+        path = os.path.join(root_path, scene, x, test_or_train)
+        return [os.path.join(path, i) for i in os.listdir(path)]
+
+    assert os.path.isdir(root_path)
+
+    render_paths = []
+    gt_paths = []
+
+    # e.g. gt_paths = ["a", "b", "c"]
+    #   render_paths = [["a1", "a2", "a3"], ...]
+
+    # [a1, b1, c1]
+    # [a2, b2, c2]
+
+    for scene in os.listdir(root_path):
+        # If scenes is specified, skip scene if not in the list
+        if scenes is not None and scene not in scenes:
+            continue
+        gt_paths += path_list(scene, "train", None)
+        gt_paths += path_list(scene, "test", None)
+
+        rpaths = []
+        for i in os.listdir(os.path.join(root_path, scene)):
+            if i.startswith("render_"):
+                it = int(i[7:])
+                if iters is None or it in iters:
+                    rpaths.append(path_list(scene, "train", it) + path_list(scene, "test", it))
+
+        render_paths += list(zip(*rpaths))
+
+    out_gt = [os.path.join(out_path, "train", "gt"), os.path.join(out_path, "test", "gt")]
+    out_rn = [os.path.join(out_path, "train", "render"), os.path.join(out_path, "test", "render")]
+
+    os.makedirs(out_gt[0], exist_ok=True)
+    os.makedirs(out_gt[1], exist_ok=True)
+    os.makedirs(out_rn[0], exist_ok=True)
+    os.makedirs(out_rn[1], exist_ok=True)
+    n = len(render_paths[0])
+    if n > 1:
+        for i in range(n):
+            os.makedirs(os.path.join(out_rn[0], str(i)), exist_ok=True)
+            os.makedirs(os.path.join(out_rn[1], str(i)), exist_ok=True)
+
+    for i, path in tqdm(enumerate(gt_paths)):
+        ind = 1 if i % split_every_n == 0 else 0
+        path2 = os.path.join(out_gt[ind], f"{i}.png")
+        shutil.copy(path, path2)
+
+    for i, paths in tqdm(enumerate(render_paths)):
+        ind = 1 if i % split_every_n == 0 else 0
+        for j, path in enumerate(paths):
+            p = f"{j}/{i}.png" if n > 1 else f"{i}.png"
+            path2 = os.path.join(out_rn[ind], p)
+            shutil.copy(path, path2)
 
 
 if __name__ == "__main__":
@@ -115,33 +160,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    dataset = NoisyDataset(root_path=args.input,
-                           iters=args.iters,
-                           scenes=args.scenes)
+    construct_subset_dataset(root_path=args.input,
+                             out_path=args.output,
+                             iters=args.iters,
+                             scenes=args.scenes,
+                             split_every_n=args.split_every_n)
 
-    out_gt = [os.path.join(args.output, "train", "gt"), os.path.join(args.output, "test", "gt")]
-    out_rn = [os.path.join(args.output, "train", "render"), os.path.join(args.output, "test", "render")]
-
-    os.makedirs(out_gt[0], exist_ok=True)
-    os.makedirs(out_gt[1], exist_ok=True)
-    os.makedirs(out_rn[0], exist_ok=True)
-    os.makedirs(out_rn[1], exist_ok=True)
-    n = len(dataset.render_paths[0])
-    if n > 1:
-        for i in range(n):
-            os.makedirs(os.path.join(out_rn[0], str(i)), exist_ok=True)
-            os.makedirs(os.path.join(out_rn[1], str(i)), exist_ok=True)
-
-    for i, path in tqdm(enumerate(dataset.gt_paths)):
-        ind = 1 if i % args.split_every_n == 0 else 0
-        path2 = os.path.join(out_gt[ind], f"{i}.png")
-        shutil.copy(path, path2)
-
-    for i, paths in tqdm(enumerate(dataset.render_paths)):
-        ind = 1 if i % args.split_every_n == 0 else 0
-        for j, path in enumerate(paths):
-            p = f"{j}/{i}.png" if n > 1 else f"{i}.png"
-            path2 = os.path.join(out_rn[ind], p)
-            shutil.copy(path, path2)
 
 
---
+++ noisy_dataset/util.py
@@ -0,0 +1,10 @@
+import torch
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+
+
+
+def evaluate(model, dataset):
+    pass
