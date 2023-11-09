d6788158ac36a24ca65de6e82d2ce4be3ffe6d4d
zzb <40760215+zzbuzzard@users.noreply.github.com>
2023-11-09 20:01:22 +0000

    Implement PSNR, SSIM evaluation on noisy dataset

--- noisy_dataset/util.py
+++ noisy_dataset/util.py
@@ -1,10 +1,37 @@
 import torch
+from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+import torch.utils.data as dutils
+from tqdm import tqdm
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
+def evaluate(model, dataset, batch_size):
+    """Returns (mean PSNR, mean SSIM) on the given dataset."""
+    dl = dutils.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    data_range = 1
+    psnr = PeakSignalNoiseRatio(data_range=data_range, dim=(1, 2, 3), reduction="sum").to(device)
+    ssim = StructuralSimilarityIndexMeasure(data_range=data_range, reduction="sum").to(device)
 
+    psnr_total = 0
+    ssim_total = 0
 
+    count = 0
 
-def evaluate(model, dataset):
-    pass
+    with torch.no_grad():
+        it = tqdm(dl)
+        for xs, ys in it:
+            xs = xs.to(device)
+            ys = ys.to(device)
+
+            ys_pred = model(xs)
+            assert torch.min(ys_pred) >= 0 and torch.max(ys_pred) <= data_range
+
+            psnr_total += psnr(ys_pred, ys)
+            ssim_total += ssim(ys_pred, ys)
+            count += xs.shape[0]
+
+            it.set_description(f"PSNR = {psnr_total / count}, SSIM = {ssim_total / count}")
+
+    return psnr_total / len(dataset), ssim_total / len(dataset)
+
